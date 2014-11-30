d452aa35bc4c00a853c6af0aa956d0210a84dc66
mahiro0210 <info@xflow.jpn.com>
2014-11-30 14:24:36 +0000

    hoge

---
+++ test.rb
@@ -0,0 +1,12 @@
+class A                                                                                             
+  def initialize                                                                                    
+    @a = "Ruby"                                                                                     
+  end                                                                                               
+                                                                                                    
+  def hello                                                                                         
+    puts @a                                                                                         
+  end                                                                                               
+end                                                                                                 
+                                                                                                    
+object = A.new                                                                                      
+object.hello    
